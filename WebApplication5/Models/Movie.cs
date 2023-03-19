f20b396d769b485ab425bf8e37c0768a6a2ea95d
Martina <klimoskamartina@gmail.com>
2023-03-19 22:38:13 +0100

    Add project files.

---
+++ WebApplication5/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    
+    public class Movie
+    {
+        [Required(), Key]
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public float Rating { get; set; }
+    }
+
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasKey(e => e.Id); // configure the key
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
